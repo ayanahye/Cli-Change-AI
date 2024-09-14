0731579f1fb518a157313857184bb7eedaa6131e
ayana <ayanahye2@gmail.com>
2024-09-13 22:50:09 -0700

    modify backend

--- CliChangeAI/CliChangeAI/settings.py
+++ CliChangeAI/CliChangeAI/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -86,6 +88,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 
 # Password validation
